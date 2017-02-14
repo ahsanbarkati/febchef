f8168cc78c94c148009c6ed3edb2305fd78ef483
ahsanbarkati <ahsanbarkati@gmail.com>
2017-02-15 03:20:43 +0530

    plzzz give a look
    totally working but misses some test cases plzz look

--- bitland.c
+++ bitland.c
@@ -7,15 +7,15 @@ int main(void) {
     scanf("%d",&w);
     for(;w--;){
         int sum=0,i,j,k,p,t,f=0,s[100000],y;
-        scanf("%d%d",&nos,&nod);
+        scanf("%d%d",&nos,&nod);			//nos is no of states and nod is no if dist in ech state
        
-       for(i=0;i<nos*nod;i++)scanf("%d",&arr[i]);
+       for(i=0;i<nos*nod;i++)scanf("%d",&arr[i]);	//taking input of the rep of ech dist
         
 
         for(i=0;i<nos*nod;i++){
              // printf("i is %d\n",i);
             sum=0;
-            for(y=0;y<nos;y++)s[y]=0;
+            for(y=0;y<nos;y++)s[y]=0;			//s is the sum of values of the dists in a state
 
             for(j=i,t=i;j<i+nos*nod;j++,t++){
                 if(t>(nos*nod-1))t=t%(nos*nod);
